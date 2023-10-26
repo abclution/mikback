77967bc028f215b828869b9dc948a5f7e3f6b476
Andreas Tyrosvoutis <andreas.tyrosvoutis@gmail.com>
2023-10-26 14:36:16 +0300

    Seperate ssh options from global to per device. Updated readme file.

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Simple backup script for multiple mikrotik devices written in Python\*
 * Please make sure ssh and scp from your shell is availiable in your path.
 * Tested and written on Debian 12
 
-This script has a global ssh option of `PubkeyAcceptedAlgoriths=ssh-rsa` which most modern Linux distros have disabled by default as it is consider insecure. ** Mikrotik devices require this for passwordless login.**
 
 # USAGE
 
@@ -34,18 +33,19 @@ This is the file where you should define your devices. It is a json file and is 
 ```json
 {
     "device1": {                          // This is the device definition, it can be any unique name.
-        "DEVICE_NAME": "Mikrotik-A",      // The device name itself, used in nameing the export and backup files. Also should be unique otherwise your files will overwrite each other.
+        "DEVICE_NAME": "Mikrotik-A",      // The device name itself, used in naming the export and backup files. Also should be unique otherwise your files will overwrite each other.
         "DEVICE_IP": "192.168.22.1",      // IP of the device
         "DEVICE_PORT": 5522,              // SSH Port of the device
         "DEVICE_USERNAME": "admin-ssh",   // Username (you should create a new user with FULL access on the device)
         "DEVICE_SSHKEY": "~/.ssh/id_rsa", // The private key used for login, the default will be your user default
+        "SSH_OPTIONS": "-oPubkeyAcceptedAlgorithms=+ssh-rsa", // Needed for most Mikrotik devices
         "BASE_PATH": "./configs/",        // Local destination root for exports and backups.
         "EXPORT": false,                  // Mikrotik supports multiple forms when exporting. This is the "basic" export. Any export enabled will export of that type. 
         "EXPORT_VERBOSE": false,          // Verbose export
         "EXPORT_TERSE": false,            // Terse export
         "EXPORT_COMPACT": false,          // Compact export
         "BACKUP": true,                   // Must be true to create and download a Mikrotik binary .backup file
-        "BACKUP_PASSWORD": "sdfggdf"      // Password used to encrypt the binary backup, set to null (no quotations) to disable encryption.
+        "BACKUP_PASSWORD": "sdfggdf"      // Password used to encrypt the binary backup, set to null (no quotations) or "" (double quotations) to disable encryption.
 
     }
 }
@@ -69,4 +69,11 @@ To disable encryption of the binary backup, `BACKUP_PASSWORD` must be set to nul
 `DEVICE_NAME`
 
 As long as all `DEVICE_NAME` are unique, the `BASE_PATH` can be the same for all devices.
+
+`SSH_OPTIONS`
+
+Mikrotik devices use older crypto libraries. The default value of `-o PubkeyAcceptedAlgoriths=ssh-rsa` should work for most Mikrotik devices. This enables a depreciated crypto which most modern Linux distros have disabled by default as it is consider insecure. ** Most Mikrotik devices require some form of this option for successful password-less login.**
+
+For even older versions of Mikrotik, you may need this instead: `"-o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedAlgorithms=+ssh-rsa"` 
+
 
--- mikback.py
+++ mikback.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python
 import datetime
 import subprocess
 import json
 
 # Uncomment this area if you want UTC time. Default is local time stamps.
 # Get the current UTC date and time
-#current_datetime = datetime.datetime.utcnow()
+# current_datetime = datetime.datetime.utcnow()
 
 # Get the local date and time
 current_datetime = datetime.datetime.now()
@@ -12,51 +13,53 @@ current_datetime = datetime.datetime.now()
 # Format the date and time
 DATESTAMP = current_datetime.strftime("%Y-%m-%d-%s")
 
-SSH_OPTIONS = "PubkeyAcceptedAlgorithms=ssh-rsa"
 
 # Load device settings from the devices configuration file
-with open('devices.json') as config_file:
+with open("devices.json") as config_file:
     devices = json.load(config_file)
 
-# Grabs the output of the Mikrotik export function and writes to local file
-def get_mikrotik_export(device_name, file_name, export_type=""):
 
+def get_mikrotik_export(device_name, file_name, export_type=""):
+    '''SSH into the device and grabs the output of the Mikrotik export function
+    and writes to local file'''
     device_settings = devices.get(device_name)
 
     if not device_settings:
         print(f"Device '{device_name}' not found in the configuration file.")
         return
 
-    sh_command = f'ssh -o {SSH_OPTIONS} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/export {export_type}"'
+    sh_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} {device_settings["SSH_OPTIONS"]} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/export {export_type}"'
 
     # Run the SSH command and capture the output to a file
-    with open(f'{device_settings["BASE_PATH"]}/{file_name}', 'w') as output_file:
+    with open(f'{device_settings["BASE_PATH"]}/{file_name}', "w") as output_file:
         try:
             subprocess.run(sh_command, shell=True, stdout=output_file, check=True)
             print(f"Command executed and output saved to {file_name}")
         except subprocess.CalledProcessError as e:
             print(f"Command failed with error: {e}")
 
-# Creates a backup of the device's configuration and saves it to a file on the
-# device. Then uses SCP to copy the file to the local machine. And finally
-# uses SSH again and removes the file from the device.
+
 def get_mikrotik_backup(device_name, file_name, backup_password=None):
+    """Creates a backup of the device's configuration and saves it to a file on the
+    device. Then uses SCP to copy the file to the local machine. And finally
+    uses SSH again and removes the file from the device."""
+
     device_settings = devices.get(device_name)
 
     if not device_settings:
         print(f"Device '{device_name}' not found in the configuration file.")
         return
 
-    if backup_password is None:
-        sh_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} -o {SSH_OPTIONS} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/system backup save dont-encrypt=yes name={file_name}"'
+    if backup_password is None or backup_password == "":
+        sh_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} {device_settings["SSH_OPTIONS"]} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/system backup save dont-encrypt=yes name={file_name}"'
     else:
-        sh_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} -o {SSH_OPTIONS} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/system backup save encryption=aes-sha256 password={backup_password} name={file_name}"'
+        sh_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} {device_settings["SSH_OPTIONS"]} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/system backup save encryption=aes-sha256 password={backup_password} name={file_name}"'
 
     # Retrieve binary backup
-    scp_command = f'scp -i {device_settings["DEVICE_SSHKEY"]} -o {SSH_OPTIONS} -P {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]}:/{file_name} {device_settings["BASE_PATH"]}'
+    scp_command = f'scp -i {device_settings["DEVICE_SSHKEY"]} {device_settings["SSH_OPTIONS"]} -P {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]}:/{file_name} {device_settings["BASE_PATH"]}'
 
     # Cleanup the backup file
-    clean_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} -o {SSH_OPTIONS} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/file remove {file_name}"'
+    clean_command = f'ssh -i {device_settings["DEVICE_SSHKEY"]} {device_settings["SSH_OPTIONS"]} -p {device_settings["DEVICE_PORT"]} {device_settings["DEVICE_USERNAME"]}@{device_settings["DEVICE_IP"]} "/file remove {file_name}"'
 
     try:
         subprocess.run(sh_command, shell=True, stdout=subprocess.DEVNULL, check=True)
@@ -72,50 +75,32 @@ def get_mikrotik_backup(device_name, file_name, backup_password=None):
         print(f"Command failed with error: {e}")
 
 
-
 for device in devices:
-    
-    if devices[device]['BACKUP'] == True:
-        get_mikrotik_backup(device, f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}.backup", devices[device]['BACKUP_PASSWORD'])
+    if devices[device]["BACKUP"] == True:
+        get_mikrotik_backup(
+            device,
+            f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}.backup",
+            devices[device]["BACKUP_PASSWORD"],
+        )
 
-    if devices[device]['EXPORT'] == True:
+    if devices[device]["EXPORT"] == True:
         get_mikrotik_export(device, f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}.rsc")
-    
-    if devices[device]['EXPORT_COMPACT'] == True:
-        get_mikrotik_export(device, f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}-COMPACT.rsc", "compact")
-    
-    if devices[device]['EXPORT_VERBOSE'] == True:
-        get_mikrotik_export(device, f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}-VERBOSE.rsc", "verbose")
-    
-    if devices[device]['EXPORT_TERSE'] == True:
-        get_mikrotik_export(device, f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}-TERSE.rsc", "terse")
 
+    if devices[device]["EXPORT_COMPACT"] == True:
+        get_mikrotik_export(
+            device,
+            f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}-COMPACT.rsc",
+            "compact",
+        )
 
+    if devices[device]["EXPORT_VERBOSE"] == True:
+        get_mikrotik_export(
+            device,
+            f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}-VERBOSE.rsc",
+            "verbose",
+        )
 
-
-
-
-    # print(f"Device: {device['DEVICE_NAME']}")
-    #print(device["DEVICE_NAME"])
-    #print(type(device))
-    #print("fart")
-    #print(f"Device: {device[2]}")
-    #print(devices[device]["DEVICE_NAME"])
-    
-# # Create and retrieve unencrypted binary backup.
-# get_mikrotik_backup(f'{DEVICE_NAME}-{DATESTAMP}.backup')
-
-# # Create and retrieve encrypted binary backup.
-# get_mikrotik_backup(f'{DEVICE_NAME}-{DATESTAMP}.ENCRYPTED.backup', BACKUP_PASSWORD)
-
-# # Create and capture standard export script.
-# get_mikrotik_export(f'{DEVICE_NAME}-{DATESTAMP}_export.rsc')
-
-# # Create and capture compact export script.
-# get_mikrotik_export(f'{DEVICE_NAME}-{DATESTAMP}_export-compact.rsc', "compact")
-
-# # Create and capture verbose export script.
-# get_mikrotik_export(f'{DEVICE_NAME}-{DATESTAMP}_export-verbose.rsc', "verbose")
-
-# # Create and capture terse export script.
-# get_mikrotik_export(f'{DEVICE_NAME}-{DATESTAMP}_export-terse.rsc', "terse")
+    if devices[device]["EXPORT_TERSE"] == True:
+        get_mikrotik_export(
+            device, f"{devices[device]['DEVICE_NAME']}_{DATESTAMP}-TERSE.rsc", "terse"
+        )
